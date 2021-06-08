3d506495a922ee912509432b43086ee847901039
Bianca Barbosa <yume_shiro@hotmail.com>
2021-06-07 23:04:02 -0300

    learning javascript: conditional, repeat, date

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+var date = new Date();
+alert(date.getDay());
+alert(date.getHours());
+
+/*
+for(var count = 0; count <= 5; count++) {
+    console.log(count);
+}
+
+var count = 0;
+while(count <= 5) {
+    console.log(count);
+    count++;
+}
+
+var idade = prompt("Qual sua idade?")
+if(idade >= 18) {
+    alert("maior de idade")
+} else {
+    alert("menor de idade")
+}
+
+dicionário
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta);
+
+var list = ["maça", "pera", "laranja"];
+list.push("uva");
+list.pop();
+
+console.log("Lista: " + list);
+console.log("Lista: " + list.toString());
+console.log(list.join("-"));
+console.log("Tamanho da lista: " + list.length);
+
+
+var name = "Bianca Barbosa";
+var age = 20;
+alert("Hello " + name + " tem " + (age + 4) + " anos");
+
+var phrase = "Japão é a melhor seleção do mundo!";
+console.log(phrase.replace("Japão", "Brasil"));*/
